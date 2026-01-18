b0f3c5cae47f8b92776196f56a348087c55720f3
umabalak <Krishnanumabala@gmail.com>
2026-01-18 21:20:46 +0530

    Created clear the clutter exercise

---
+++ beginner/Exercise 7/1.py
@@ -0,0 +1,29 @@
+import os
+folder_path = r"C:\Users\ubkk\OneDrive\Documents\python-mastery\beginner\Exercise 7"
+
+files = os.listdir(folder_path)                    #get all the files in the folder
+file_count = {}                                                                                              #dict to store count of each file type
+
+for file in files:
+    name, extension = os.path.splitext(file)                                                          #spilt filename and extension 
+    
+    if extension == "":                                 #skip folder or file without extension
+        continue
+
+#Initialze count for new extension
+    if extension not in file_count:
+        file_count[extension] = 1
+    else:
+        file_count[extension]  += 1
+    
+    new_name = f"{file_count[extension]}{extension}"              #create new file name
+
+
+    #full old and new paths
+    old_path = os.path.join(folder_path, file)
+    new_path = os.path.join(folder_path, new_name)
+
+    os.rename(old_path, new_path)                       #rename the file
+
+print("Folder clean sucessfully")
+
